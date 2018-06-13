2343fe1afe7d58edd1b57f3e72a11666b83144ed
Vinícius Thiengo <viniciusthiengo@gmail.com>
2018-06-13 15:24:26 -0300

    Update

---
+++ app/src/main/java/thiengo/com/br/torneioscontinentais/domain/Jogo.kt
@@ -0,0 +1,9 @@
+package thiengo.com.br.torneioscontinentais.domain
+
+
+class Jogo(
+    val timeCasa: Time,
+    val timeVisitante: Time,
+    val estadio: String,
+    val data: String,
+    val horario: String )
